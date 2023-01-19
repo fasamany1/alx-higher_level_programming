5fb75010f8de1b233c2b9bad999c89c815b3585f
Forster Asamany <asamanyforster@yahoo.com>
2023-01-19 08:47:32 +0000

    Update README.md

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for values in sorted(a_dictionary):
-        print('{}: {}'.format(values, a_dictionary[values]))
--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Filename:  0-square"""
-
-
-class Square:
-    """An empty class Square that defines a square:"""
-    pass
