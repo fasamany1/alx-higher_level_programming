fbfa151bf6b897b00abba8dd69cd215dc3216240
Forster Asamany <asamanyforster@yahoo.com>
2023-04-02 06:30:30 +0100

    Use Github API to display an ID

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Uses the GitHub API to display a GitHub ID based on given credentials
-  - Uses Basic Authentication to access the ID
 """
-import sys
-import requests
-from requests.auth import HTTPBasicAuth
+Take your GitHub credentials (username and password) and use the GitHub API
+to display your id
+"""
 
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    username = argv[1]
+    password = argv[2]
+
+    URL = "https://api.github.com/user"
+    response = get(URL, auth=(username, password))
+    json = response.json()
+
+    print(json.get('id'))
