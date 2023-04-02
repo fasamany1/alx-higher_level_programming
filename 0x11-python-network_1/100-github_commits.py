d2fededcf5cb575e9cf1e1e111aaccf61a62a871
Forster Asamany <asamanyforster@yahoo.com>
2023-04-02 06:33:45 +0100

    Take 2 arguments to solve a challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository
-"""
-import sys
-import requests
+"""Take 2 arguments in order to solve a challenge"""
 
 
-if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    repo = argv[1]
+    owner = argv[2]
+    i = 0
+
+    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    response = get(URL)
+    json = response.json()
+
+    for element in json:
+        if i > 9:
+            break
+        sha = element.get('sha')
+        author = element.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
+        i += 1
