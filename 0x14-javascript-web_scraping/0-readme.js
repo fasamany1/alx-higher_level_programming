26e59496125d9237bfc305b39d8207938082da94
Forster Asamany <asamanyforster@yahoo.com>
2023-04-25 12:55:42 +0000

    Read and print the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 // Load the necessary modules
 const fs = require('fs');
 // Get the file path from command line arguments
-const filePath = process.argv[2];
+const filename = process.argv[2];
 
 // Read the contents of the file
-fs.readFile(filePath, 'utf-8', (error, content) => {
+fs.readFile(filename, 'utf-8', (error, content) => {
   if (error) {
       // Print the error object if an error occurred
       console.log(error);
