ee37fb459e8c180c3ebf321ffda8b332cf189420
Forster Asamany <asamanyforster@yahoo.com>
2023-04-25 16:16:01 +0000

    Print the title of a movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,8 +5,8 @@ const starWarsUrl = `https://swapi-api.alx-tools.com/api/films/`.concat(process.
 
 // Make a request to the specified URL
 request(starWarsUrl, function (_err, _res, body) {
-    // Parse the response body as JSON
-    body = JSON.parse(body);
-    // Print the movie title
-    console.log(body.title);
+	// Parse the response body as JSON
+	body = JSON.parse(body);
+	// Print the movie title
+	console.log(body.title);
 });
