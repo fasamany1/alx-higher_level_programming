2073cd380d2b99cab7c5a97294000b2c63a0a221
Forster Asamany <asamanyforster@yahoo.com>
2023-04-25 12:56:27 +0000

    Write a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,10 @@
 
 "use strict";
 const fs = require('fs');
-const filePath = process.argv[2];
+const filename = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (error) => {
+fs.writeFile(filename, content, 'utf-8', (error) => {
   if (error) {
     console.log(error);
   }
