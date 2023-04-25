ee942e023ff3cb99d16eacfaffa41553f34abc53
Forster Asamany <asamanyforster@yahoo.com>
2023-04-25 14:35:44 +0000

    Get the contents of a webpage

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+// Importing required modules
+const request = require('request');
+const fs = require('fs');
+// Reading command line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Making GET request to the given URL
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    // Writing the response to the file
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
